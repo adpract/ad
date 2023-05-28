1d5ecc756bfd77ccf6590088b470fb0499dc97cd
Pratik Koratkar <86914769+pratikkoratkar@users.noreply.github.com>
2023-05-28 19:55:42 +0530

    quote share

---
+++ 2fragmentsharedata/app/src/main/java/com/example/a2fragmentsharedata/firstFragment.java
@@ -0,0 +1,93 @@
+package com.example.a2fragmentsharedata;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link firstFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class firstFragment extends Fragment implements View.OnClickListener {
+
+    View rootView;
+    EditText myQuote;
+    Button b1;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+
+    public firstFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment firstFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static firstFragment newInstance(String param1, String param2) {
+        firstFragment fragment = new firstFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        rootView = inflater.inflate(R.layout.fragment_first, container, false);
+        initView();
+        return rootView;
+    }
+
+    private void initView() {
+        myQuote = rootView.findViewById(R.id.quote);
+        Button b1 = rootView.findViewById(R.id.submit);
+        b1.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.submit){
+            Bundle bundle = new Bundle();
+            String strquote = myQuote.getText().toString();
+            bundle.putString("myQuote" , strquote);
+            secondFragment secondFragment = new secondFragment();
+            secondFragment.setArguments(bundle);
+            getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.container , secondFragment).commit();
+
+        }
+
+    }
+}
